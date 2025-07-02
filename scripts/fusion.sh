ee474bd46d7f3bed70f69196b1a2b36cec280580
Fang Zhaojie <81013433+Tinysqua@users.noreply.github.com>
2025-07-02 14:47:10 +0800

    Update fusion.sh

--- scripts/fusion.sh
+++ scripts/fusion.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nohup python 2_fusion.py --mode fuse --in_dir /ssd_data/shizhen/Aidite_Crown_Dataset_scale_second/tooth_crown --depth_dir /ssd_data/shizhen/Aidite_Crown_Dataset_scale_second/tooth_crown_depth --out_dir /ssd_data/shizhen/Aidite_Crown_Dataset_scale_second/tooth_crown_watertight > fuse_output.log 2>&1 &
+nohup python 2_fusion_diy.py --mode fuse --in_dir /data_new2/shizhen/JustTest/Aidite_Crown_Dataset_sixth_align/tooth_crown \
+--depth_dir /data_new2/shizhen/JustTest/Aidite_Crown_Dataset_sixth_align/tooth_crown_depth \
+--out_dir /data_new2/shizhen/JustTest/Aidite_Crown_Dataset_sixth_align/tooth_crown_watertight \
+--resolution 512 > fuse_output.log 2>&1 &
